57562c2679b37daf6b9ff2bc51864be14f0bb968
anagromataf <info@tobias-kraentzer.de>
2011-08-15 14:33:39 +0200

    Add UI from the API project.

---
+++ Sources/SoundCloudUI/SCAppIsRunningOnIPad.h
@@ -0,0 +1,9 @@
+//
+//  SCAppIsRunningOnIPad.h
+//  SoundCloudAPI
+//
+//  Created by Tobias Kräntzer on 22.07.11.
+//  Copyright 2011 nxtbgthng. All rights reserved.
+//
+
+BOOL SCAppIsRunningOnIPad();
