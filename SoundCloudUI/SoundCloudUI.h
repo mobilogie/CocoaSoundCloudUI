a1352681d193520cf68c4718122ad15647245e01
anagromataf <info@tobias-kraentzer.de>
2011-08-15 12:17:59 +0200

    Initial Commit

---
+++ SoundCloudUI/SoundCloudUI.h
@@ -0,0 +1,13 @@
+//
+//  SoundCloudUI.h
+//  SoundCloudUI
+//
+//  Created by Tobias Kräntzer on 15.08.11.
+//  Copyright 2011 nxtbgthng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundCloudUI : NSObject
+
+@end
---
+++ SoundCloudUI/SoundCloudUI.m
@@ -0,0 +1,23 @@
+//
+//  SoundCloudUI.m
+//  SoundCloudUI
+//
+//  Created by Tobias Kräntzer on 15.08.11.
+//  Copyright 2011 nxtbgthng. All rights reserved.
+//
+
+#import "SoundCloudUI.h"
+
+@implementation SoundCloudUI
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
